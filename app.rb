bb1fa9938876ded29fd30f0166b1ca78448573cc
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-07-08 17:34:53 +0100

    sinatra: Keeping views clean 2.0

--- app.rb
+++ app.rb
@@ -4,16 +4,13 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/' do
-  @random_name = ["Ted", "Anastasiia", "Cat"].sample
-  erb(:index)
+  "Hello!"
 end
 
 get '/secret' do
   "welcome to this secret page ;)"
 end
 
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src= 'http://bit.ly/1eze8aE' style='width:100%'>
-  </div>"
+get '/random-cat' do
+    erb(:index)
 end
